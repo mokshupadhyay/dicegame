5c6bf561a5ce4ce03ef238632bd3db22312ce982
Moksh Upadhyay <83757186+mokshupadhyay@users.noreply.github.com>
2023-04-25 16:23:41 +0530

    update index js

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var randomnumber1 = Math.floor(Math.random() * 6 ) + 1 ;
+function rollDice() {
+    var randomnumber1 = Math.floor(Math.random() * 6 ) + 1 ;
 
-var randomDiceImage1 = "dice" + randomnumber1 + ".png" ;
+    var randomDiceImage1 = "dice" + randomnumber1 + ".png" ;
 
-var image1 = document.querySelectorAll("img")[0];
+    var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",randomDiceImage1);
+    image1.setAttribute("src",randomDiceImage1);
 
-var randomnumber2 = Math.floor(Math.random() * 6 ) + 1 ;
+    var randomnumber2 = Math.floor(Math.random() * 6 ) + 1 ;
 
-var randomDiceImage2 = "dice" + randomnumber2 + ".png";
+    var randomDiceImage2 = "dice" + randomnumber2 + ".png";
 
-var image1 = document.querySelectorAll("img")[1] ;
+    var image1 = document.querySelectorAll("img")[1] ;
 
-image1.setAttribute("src",randomDiceImage2);
+    image1.setAttribute("src",randomDiceImage2);
 
-if (randomnumber1 > randomnumber2) {
-    document.querySelector("h1").innerHTML=("🏆Player1 Wins!");   
+    if (randomnumber1 > randomnumber2) {
+        document.querySelector("h1").innerHTML=("🏆Player1 Wins!");   
+    }
+    else if (randomnumber1 < randomnumber2) {
+        document.querySelector("h1").innerHTML=("Player2 Wins!🏆");   
+    }
+    else{
+        document.querySelector("h1").innerHTML=("It's a Draw");   
+
+    }
 }
-else if (randomnumber1 < randomnumber2) {
-    document.querySelector("h1").innerHTML=("Player2 Wins!🏆");   
-}
-else{
-    document.querySelector("h1").innerHTML=("It's a Draw");   
 
-}
