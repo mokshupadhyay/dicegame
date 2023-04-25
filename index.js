dd7ce6ac656a814d107408f066624643e8d6913d
Moksh Upadhyay <83757186+mokshupadhyay@users.noreply.github.com>
2023-04-25 14:10:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomnumber1 = Math.floor(Math.random() * 6 ) + 1 ;
 
-var randomDiceImage1 = "images/dice" + randomnumber1 + ".png" ;
+var randomDiceImage1 = "dice" + randomnumber1 + ".png" ;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -8,7 +8,7 @@ image1.setAttribute("src",randomDiceImage1);
 
 var randomnumber2 = Math.floor(Math.random() * 6 ) + 1 ;
 
-var randomDiceImage2 = "images/dice" + randomnumber2 + ".png";
+var randomDiceImage2 = "dice" + randomnumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[1] ;
 
