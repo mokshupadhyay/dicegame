60f12dccbed550e3935a07425d0379ff462aad7b
Moksh Upadhyay <83757186+mokshupadhyay@users.noreply.github.com>
2023-04-25 12:47:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomnumber1 = Math.floor(Math.random() * 6 ) + 1 ;
+
+var randomDiceImage1 = "images/dice" + randomnumber1 + ".png" ;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomDiceImage1);
+
+var randomnumber2 = Math.floor(Math.random() * 6 ) + 1 ;
+
+var randomDiceImage2 = "images/dice" + randomnumber2 + ".png";
+
+var image1 = document.querySelectorAll("img")[1] ;
+
+image1.setAttribute("src",randomDiceImage2);
+
+if (randomnumber1 > randomnumber2) {
+    document.querySelector("h1").innerHTML=(" 🏆 Player1 Wins!");   
+}
+else if (randomnumber1 < randomnumber2) {
+    document.querySelector("h1").innerHTML=(" 🏆 Player2 Wins!");   
+}
+else{
+    document.querySelector("h1").innerHTML=("Draw");   
+
+}
